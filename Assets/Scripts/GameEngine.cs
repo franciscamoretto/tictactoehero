777b5ab1edbea46949a3b6d3173f414c2d08d1e6
Claudio Sampaio <claudio.sampaio@gmail.com>
2018-05-27 14:08:23 -0400

    Revert "modificacoes nos popup da hud e na media"
    This reverts commit 2cde79f7b853a6f83bf650593f72e3ad490a2027.

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -25,6 +25,7 @@ public class GameEngine : MonoBehaviour {
         if (instance == null)
         {
             instance = this;
+            FindObjectOfType<AudioManager>().Play("menubselect");
         }
         else if (instance != this)
         {
@@ -64,7 +65,7 @@ public class GameEngine : MonoBehaviour {
     {
         board[row + (NUM_ROW_COL * arena.row), col + (NUM_ROW_COL * arena.col)] = brasao;
         CheckMatchLine(arena, row, col);
-        
+        FindObjectOfType<AudioManager>().Play("menubselect");
 
     }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,20 +10,20 @@ public class GameManager : MonoBehaviour {
     private const int OFFSET = 10;
     public enum GAMELMODE { quick, normal, hero };
 
-    public GameObject arenaPrefab;
     public GameObject quickBoardPrefab;
     public GameObject normalBoardPrefab;
     public GameObject heroBoardPrefab;
     public bool isGameEnds = false;
     public GAMELMODE gameMode;
+    public GameObject DrawMessagePrefab;
 
     // Jogador que está na vez
     private Player player;
     private Player[] players = new Player[2];
     private List<GameObject> playerTurnMsg = new List<GameObject>();
-    private Transform mainCanvas;
     private GameObject board;
     private GameObject winMessage;
+    private Transform boardArea;
 
 
     //Awake is always called before any Start functions
@@ -74,8 +74,8 @@ public class GameManager : MonoBehaviour {
             {
                 this.players[p.order] = p;
             }
-            this.mainCanvas = GameObject.FindGameObjectWithTag("MainCanvas").transform;
-            
+            this.boardArea = GameObject.FindGameObjectWithTag("MainCanvas").transform;            
+            FindObjectOfType<AudioManager>().Play("menubselect");
 
             NewGame();
         }
@@ -92,7 +92,7 @@ public class GameManager : MonoBehaviour {
         {
             player.CleanScore();
             player.timer.totalTime = time;
-            GameObject obj = Instantiate(player.TurnMessagePrefab, this.mainCanvas);
+            GameObject obj = Instantiate(player.TurnMessagePrefab, this.boardArea);
             this.playerTurnMsg.Add(obj);
         }
         this.player = this.players[0];
@@ -105,17 +105,21 @@ public class GameManager : MonoBehaviour {
     {
         if (this.gameMode.Equals(GAMELMODE.quick))
         {
-            this.board = Instantiate(quickBoardPrefab, mainCanvas);
+            this.board = Instantiate(quickBoardPrefab, this.boardArea);
+            FindObjectOfType<AudioManager>().Play("MainTheme");
         }
         else if (this.gameMode.Equals(GAMELMODE.normal))
         {
-            this.board = Instantiate(normalBoardPrefab, mainCanvas);
+            this.board = Instantiate(normalBoardPrefab, this.boardArea);
+            FindObjectOfType<AudioManager>().Play("MainTheme");
         }
         else
         {
-            this.board = Instantiate(heroBoardPrefab, mainCanvas);
+            FindObjectOfType<AudioManager>().Play("MainTheme");
+            
+            this.board = Instantiate(heroBoardPrefab, this.boardArea);
         }
-        this.board.transform.SetSiblingIndex(1);
+        this.board.transform.SetAsFirstSibling();
         InitGame();
     }
 
@@ -132,16 +136,16 @@ public class GameManager : MonoBehaviour {
         if (this.players[0].score > this.players[1].score)
         {
             Debug.Log("Player 1 Wins");
-            this.winMessage = Instantiate(this.players[0].WinMessagePrefab, this.mainCanvas);
+            this.winMessage = Instantiate(this.players[0].WinMessagePrefab, this.boardArea);
 
         } else if (this.players[0].score < this.players[1].score)
         {
             Debug.Log("Player 2 Wins");
-            this.winMessage = Instantiate(this.players[1].WinMessagePrefab, this.mainCanvas);
+            this.winMessage = Instantiate(this.players[1].WinMessagePrefab, this.boardArea);
 
         } else
         {
-            // TODO: Draw message
+            this.winMessage = Instantiate(this.DrawMessagePrefab, this.boardArea);
         }
         
     }
@@ -173,6 +177,20 @@ public class GameManager : MonoBehaviour {
         this.player.timer.ResetTimer();
         this.player.timer.StartTimer();
     }
+
+    /// <summary>
+    /// Pausa o timer do jogador
+    /// </summary>
+    public void PauseGame(bool pause)
+    {
+        if (pause) {
+            this.player.timer.PauseTimer();
+        } else
+        {
+            this.player.timer.StartTimer();
+        }
+    }
+
 
     /// <summary>
     /// Altera o player que está jogando
